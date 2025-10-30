8bfb3b442b5dd1c3171539a9f6ac135426a5e026
maxipriani <maxipriani2002@gmail.com>
2025-10-30 21:28:11 +0000

    refactor(user): remove unnecessary id setter

--- src/main/java/com/maxi/marketplace/user/model/UserEntity.java
+++ src/main/java/com/maxi/marketplace/user/model/UserEntity.java
@@ -29,7 +29,6 @@ public class UserEntity {
     public String getName() { return name; }
     public String getEmail() { return email; }
 
-    public void setId(Long id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
 
